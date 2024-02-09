d4932384834849ba18e4c252dbcce49c4fcf80ad
Hidetake Iwata <int128@gmail.com>
2024-02-09 15:17:58 +0900

    Clean up active deployment (#469)
    * Clean up active deployment
    
    * Generated by GitHub Actions (ts / generate)
    
    https://github.com/int128/delete-deployments-action/actions/runs/7840234819
    
    ---------
    
    Co-authored-by: update-generated-files-action <41898282+github-actions[bot]@users.noreply.github.com>

--- src/run.ts
+++ src/run.ts
@@ -3,6 +3,8 @@ import * as github from '@actions/github'
 import { GetDeploymentsQuery } from './generated/graphql'
 import { getDeployments } from './queries/getDeployments'
 import { deleteDeployment } from './queries/deleteDeployment'
+import { DeploymentState } from './generated/graphql-types'
+import assert from 'assert'
 
 type Inputs = {
   token: string
@@ -20,28 +22,49 @@ export const run = async (inputs: Inputs): Promise<void> => {
   core.info(JSON.stringify(deployments, undefined, 2))
   core.endGroup()
 
-  const outdatedIDs = findOutdated(deployments)
-  core.info(`Deleting outdated ${outdatedIDs.length} deployment(s)`)
+  const outdatedDeployments = findOutdated(deployments)
+  core.info(`Deleting outdated ${outdatedDeployments.length} deployment(s)`)
+  for (const outdated of outdatedDeployments) {
+    if (outdated.state === DeploymentState.Active) {
+      // An active deployment cannot be deleted directly.
+      // https://docs.github.com/en/rest/deployments/deployments?apiVersion=2022-11-28#delete-a-deployment
+      core.info(`Creating an inactive status before deleting the active deployment ${outdated.databaseId}`)
+      await octokit.rest.repos.createDeploymentStatus({
+        owner: github.context.repo.owner,
+        repo: github.context.repo.repo,
+        deployment_id: outdated.databaseId,
+        state: 'inactive',
+      })
+    }
 
-  for (const id of outdatedIDs) {
-    core.info(`Deleting ${id}`)
-    await deleteDeployment(octokit, { id })
+    core.info(`Deleting the deployment ${outdated.id}`)
+    await deleteDeployment(octokit, { id: outdated.id })
   }
 }
 
-export const findOutdated = (q: GetDeploymentsQuery) => {
-  if (q.repository == null) {
-    throw new Error(`q.repository === ${String(q.repository)}`)
-  }
+type OutdatedDeployment = {
+  id: string
+  databaseId: number
+  state: DeploymentState
+}
 
-  const outdatedIDs: string[] = []
+export const findOutdated = (q: GetDeploymentsQuery): OutdatedDeployment[] => {
+  assert(q.repository != null)
+
+  const outdated: OutdatedDeployment[] = []
   for (const node of q.repository.deployments.nodes ?? []) {
     if (node == null) {
       continue
     }
+    if (node.databaseId == null) {
+      continue
+    }
+    if (node.state == null) {
+      continue
+    }
     if (node.ref == null) {
-      outdatedIDs.push(node.id)
+      outdated.push({ id: node.id, databaseId: node.databaseId, state: node.state })
     }
   }
-  return outdatedIDs
+  return outdated
 }
