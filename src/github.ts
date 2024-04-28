0698f3f48d295d7ca1e8625ba0161664d95278df
Hidetake Iwata <int128@gmail.com>
2024-04-28 21:07:21 +0900

    Refactor getOctokit() (#542)
    * Refactor getOctokit()
    
    * Fix

---
+++ src/github.ts
@@ -0,0 +1,6 @@
+import * as github from '@actions/github'
+import { retry } from '@octokit/plugin-retry'
+
+export type Octokit = ReturnType<typeof github.getOctokit>
+
+export const getOctokit = (token: string): Octokit => github.getOctokit(token, {}, retry)
